613603df0279ca69f926638b9249441b4a3fb613
KiranBai <157038014+KiranBai@users.noreply.github.com>
2024-07-30 22:02:49 +0500

    Create Vedio2.js
    Vedio2.js Learning

---
+++ Vedio2.js
@@ -0,0 +1,5 @@
+console.log("Kiran")
+// Java script is used as frontend as well as backend web development
+// Node js, Deno js  is a environment which is used to excutes the javascript files. 
+// First we need to install Nodejs either LTS is for Product Features or Current for learning purpose.
+//Once we installed Node js then we make js file Enter this line console.log("Kiran") and then open view then terminal first you see version of node js Enter Node -v on terminal to see terminal version then put executing file name on terminal Node Vedio.js then i get output kiran on terminal. 
