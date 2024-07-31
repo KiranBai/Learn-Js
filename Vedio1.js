798c5acdb8af88b7e77e1823b5726bc48f38697a
KiranBai <157038014+KiranBai@users.noreply.github.com>
2024-07-31 17:20:18 +0000

    Learning Github Environment

--- Vedio1.js
+++ Vedio1.js
@@ -1,9 +1,9 @@
-In this vedio Lecture I have learned about why JavaScript is necessary ?
-  JavaScript is essential because it enables interactive and dynamic content on websites, enhancing user experience. 
-What we need to learn it ?
-  JavaScript is essential because it makes websites interactive and lively, turning simple pages into exciting experiences. Learning it opens up many opportunities in web and app development.
-What configuration of laptop required to learn JavaScript?
-  At any Configuration of laptop we can easy learn javascript it uses simple framework.
-What are the necessary tools are Required to leearn java Script?
-  We can learn javascript using Vscode , sublime text , code editior etc.
-Lets Start JavaScript with code and Chai
+// In this vedio Lecture I have learned about why JavaScript is necessary ?
+//   JavaScript is essential because it enables interactive and dynamic content on websites, enhancing user experience. 
+// What we need to learn it ?
+//   JavaScript is essential because it makes websites interactive and lively, turning simple pages into exciting experiences. Learning it opens up many opportunities in web and app development.
+// What configuration of laptop required to learn JavaScript?
+//   At any Configuration of laptop we can easy learn javascript it uses simple framework.
+// What are the necessary tools are Required to leearn java Script?
+//   We can learn javascript using Vscode , sublime text , code editior etc.
+// Lets Start JavaScript with code and Chai
---
+++ Vedio3.js
@@ -0,0 +1,5 @@
+// Learning about github how to make repository , git is applicable for free and paid version, paid version is used by companines
+// Once we made the repository then we click on code button where we found create codespace for getting virtual version of Vscode
+// we need to makhe Node js like version so we go to file then view then command palette write container  then we add dev container where we add nodejs javascript container
+// Once we write the code how to commit first we go to source control where we add file where we do changes by + icon and want to commit it then and we go to triple dots then push it with message commit.
+ 
