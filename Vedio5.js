6bcc2ed84972262a3b87ffb75009ffcd5212c9a7
KiranBai <kiranbai10091999@gmail.com>
2024-08-01 15:03:17 +0500

    DataType

---
+++ Vedio5.js
@@ -0,0 +1,30 @@
+"use strict"; // Java Script code with newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+//Orginal documentation of javascript in TC39.es ECMA Scrit
+console.log(3 + 3) // code readability 
+
+console.log("Kiran")
+
+
+let name = "Kiran"
+let age = 23
+let isLoggedIn = false
+let state;
+
+
+// Range
+// number => 2 to power 53
+// bigint is used in trading stock marketing
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Kiran"); // String
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
