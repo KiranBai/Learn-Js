c69cd5299f814b548f2d12ad46638a1c3c37c302
KiranBai <kiranbai10091999@gmail.com>
2024-08-02 19:13:54 +0500

    Learning about Object in Part 3 Destructing of Object and Json APT

---
+++ Vedio18.js
@@ -0,0 +1,26 @@
+// Destucturing of Objects
+const course = {
+    coursename: "Frontend in hindi",
+    price: "Free",
+    courseInstructor: "Muhammad Shakir"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "Kiran",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+// How to Json From API
+// It gives that how many object , its info using RandomUserAPI
+[
+    {},
+    {},
+    {}
+]
