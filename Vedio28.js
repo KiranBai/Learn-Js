a46cdf3547590682969b8fe55c678d1e40431879
KiranBai <kiranbai10091999@gmail.com>
2024-08-12 11:07:10 +0500

    While and Do While

---
+++ Vedio28.js
@@ -0,0 +1,20 @@
+let index = 0
+// while (index <= 20) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 3
+// }
+
+let myArray = ['Apple', "Banana", "Mango"]
+
+let arr = 0
+while (arr < myArray.length) {
+    //console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+let grade = 96
+
+do {
+    console.log(`Grade is ${grade}`);
+    grade++
+} while (grade <= 10);
